85cba4515280e72d1d44e4f014da9d600cf14e96
Isaac Secundino <isaacsecundino3@gmail.com>
2024-10-09 16:48:43 -0600

    Complete initial logical operators: and, or, equals, not_equals

--- operators/logic.go
+++ operators/logic.go
@@ -16,8 +16,38 @@ var andOperator = OperatorRunnable{
 		for i := range childs {
 			child := &(childs)[i]
 
+			fmt.Printf(
+				"Evaluating exp [%v][ %v ] \n",
+				i,
+				child.CommulativeEval.ToString(),
+			)
+
 			if child.CommulativeEval == parser.False {
 				n.CommulativeEval = parser.False
+				break
+			}
+		}
+
+		return n.CommulativeEval
+	},
+}
+
+var orEvaluator = OperatorRunnable{
+	Token: Or,
+	Evaluate: func(n *parser.Node) parser.EvalResult {
+		childs := *n.Childrens
+
+		if len(childs) > 0 {
+			n.CommulativeEval = parser.False
+		} else {
+			n.CommulativeEval = parser.True
+		}
+
+		for i := range childs {
+			child := &(childs)[i]
+
+			if child.CommulativeEval == parser.True {
+				n.CommulativeEval = parser.True
 				break
 			}
 		}
@@ -59,7 +89,42 @@ var equalsOperator = OperatorRunnable{
 	},
 }
 
+var notEqualsEvaluator = OperatorRunnable{
+	Token: NotEquals,
+	Evaluate: func(n *parser.Node) parser.EvalResult {
+		childs := *n.Childrens
+
+		if len(childs) != 2 {
+			fmt.Printf("Cannot evaluate expression with %d arguments, expected 2 \n", len(childs))
+			n.CommulativeEval = parser.False
+		} else {
+
+			first := childs[0].Token
+			second := childs[1].Token
+
+			if first != second {
+				n.CommulativeEval = parser.True
+			} else {
+				n.CommulativeEval = parser.False
+			}
+
+			fmt.Printf(
+				"Evaluating [ (%s) %v == (%s) %v ] -> %v \n",
+				reflect.TypeOf(first),
+				first,
+				reflect.TypeOf(second),
+				second,
+				n.CommulativeEval.ToString(),
+			)
+		}
+
+		return n.CommulativeEval
+	},
+}
+
 func init() {
 	operatorMap[And] = andOperator
+	operatorMap[Or] = orEvaluator
 	operatorMap[Equals] = equalsOperator
+	operatorMap[NotEquals] = notEqualsEvaluator
 }
--- operators/logic_test.go
+++ operators/logic_test.go
@@ -6,54 +6,171 @@ import (
 	"github.com/IsaacSec/go-jsonlogic/parser"
 )
 
-/*
-EQUALS evaluation, same type equal value -> 53 == 53
-*/
-func TestEqualOnSameTypeAndValue(t *testing.T) {
+/***********************
+ *		EQUALS
+ ***********************/
+
+// EQUALS evaluation, same type equal value -> 53 == 53
+func TestEqualsOnSameTypeAndValue(t *testing.T) {
 	expression := buildSimpleExp("==", 53, 53)
 
-	res := equalsOperator.Evaluate(&expression)
-	if res != parser.True {
-		t.Errorf("Expected True but got '%v'", res.ToString())
-	}
+	assertExpression(t, expression, equalsOperator, parser.True)
 }
 
-/*
-EQUALS evaluation, different type equal value -> 53 == "53"
-*/
-func TestEqualOnDiffType(t *testing.T) {
+// EQUALS evaluation, different type equal value -> 53 == "53"
+func TestEqualsOnDiffType(t *testing.T) {
 	expression := buildSimpleExp("==", 53, "fifty three")
 
-	res := equalsOperator.Evaluate(&expression)
-	if res != parser.False {
-		t.Errorf("Expected False but got '%v'", res.ToString())
-	}
+	assertExpression(t, expression, equalsOperator, parser.False)
 }
 
-/*
-EQUALS evaluation, different value -> 3421 == -123
-*/
-func TestEqualOnDiffValue(t *testing.T) {
+// EQUALS evaluation, different value -> 3421 == -123
+func TestEqualsOnDiffValue(t *testing.T) {
 	expression := buildSimpleExp("==", 3421, -123)
 
-	res := equalsOperator.Evaluate(&expression)
-	if res != parser.False {
-		t.Errorf("Expected False but got '%v'", res.ToString())
-	}
+	assertExpression(t, expression, equalsOperator, parser.False)
 }
 
 // Todo: Add conversion type when possible i.e. -> 1 == "1" == 1.0
 
-/*
-EQUALS evaluation, different type and equal value -> 42 == 42.0
-*/
-func TestEqualOnDiffTypeWithSameValue(t *testing.T) {
+// EQUALS evaluation, different type and equal value -> 42 == 42.0
+func TestEqualsOnDiffTypeWithSameValue(t *testing.T) {
 	expression := buildSimpleExp("==", 42, 42.0)
 
-	res := equalsOperator.Evaluate(&expression)
-	if res != parser.False {
-		t.Errorf("Expected False but got '%v'", res.ToString())
+	assertExpression(t, expression, equalsOperator, parser.False)
+}
+
+/***********************
+ *		NOT EQUALS
+ ***********************/
+
+// NOT EQUALS evaluation, same type and different value -> 35 != 53
+func TestNotEqualsWithDiffValue(t *testing.T) {
+	expression := buildSimpleExp("!=", 35, 53)
+
+	assertExpression(t, expression, notEqualsEvaluator, parser.True)
+}
+
+// NOT EQUALS evaluation, same type and equal value -> 53 != 53
+func TestNotEqualsWithSameValue(t *testing.T) {
+	expression := buildSimpleExp("!=", 53, 53)
+
+	assertExpression(t, expression, notEqualsEvaluator, parser.False)
+}
+
+/***********************
+ *		AND
+ ***********************/
+
+// AND evaluation, empty list -> [] = true
+func TestAndWithEmptyList(t *testing.T) {
+	expression := buildGroupExp(
+		"and",
+	)
+
+	assertExpression(t, expression, andOperator, parser.True)
+}
+
+// AND evaluation, one true and multiple false -> [true, false, false] = false
+func TestAndWithMultipleFalseAndAtLeastOneTrue(t *testing.T) {
+	expression := buildGroupExp(
+		"and",
+		parser.True,
+		parser.False,
+		parser.False,
+	)
+
+	assertExpression(t, expression, andOperator, parser.False)
+}
+
+// AND evaluation, all true -> [true, true, true] = true
+func TestAndWithAllTrue(t *testing.T) {
+	expression := buildGroupExp(
+		"and",
+		parser.True,
+		parser.True,
+		parser.True,
+	)
+
+	assertExpression(t, expression, andOperator, parser.True)
+}
+
+// AND evaluation, all false -> [false, false, false] = false
+func TestAndWithAllFalse(t *testing.T) {
+	expression := buildGroupExp(
+		"and",
+		parser.False,
+		parser.False,
+		parser.False,
+	)
+
+	assertExpression(t, expression, andOperator, parser.False)
+}
+
+/***********************
+ *		OR
+ ***********************/
+
+// OR evaluation, empty list -> [] = true
+func TestOrWithEmptyList(t *testing.T) {
+	expression := buildGroupExp(
+		"or",
+	)
+
+	assertExpression(t, expression, orEvaluator, parser.True)
+}
+
+// OR evaluation, one true and multiple false -> [false, true, false] = true
+func TestOrWithMultipleFalseAndAtLeastOneTrue(t *testing.T) {
+	expression := buildGroupExp(
+		"or",
+		parser.False,
+		parser.True,
+		parser.False,
+	)
+
+	assertExpression(t, expression, orEvaluator, parser.True)
+}
+
+// AND evaluation, all true -> [true, true, true] = true
+func TestOrWithAllTrue(t *testing.T) {
+	expression := buildGroupExp(
+		"or",
+		parser.True,
+		parser.True,
+		parser.True,
+	)
+
+	assertExpression(t, expression, orEvaluator, parser.True)
+}
+
+// AND evaluation, all false -> [false, false, false] = false
+func TestOrWithAllFalse(t *testing.T) {
+	expression := buildGroupExp(
+		"or",
+		parser.False,
+		parser.False,
+		parser.False,
+	)
+
+	assertExpression(t, expression, orEvaluator, parser.False)
+}
+
+func buildGroupExp(op parser.Token, results ...parser.EvalResult) parser.Node {
+	var expressions = make([]parser.Node, len(results))
+
+	for i, res := range results {
+		expressions[i] = parser.Node{
+			Token:           res.ToString() == "True",
+			CommulativeEval: res,
+			Kind:            parser.PrimitiveVal,
+			Childrens:       nil,
+		}
 	}
+
+	var group = parser.Node{Token: op, Kind: parser.Operator, Childrens: &expressions}
+
+	return group
 }
 
 func buildSimpleExp(op parser.Token, a parser.Token, b parser.Token) parser.Node {
@@ -70,5 +187,13 @@ func buildSimpleExp(op parser.Token, a parser.Token, b parser.Token) parser.Node
 				Kind:  parser.PrimitiveVal,
 			},
 		},
+	}
+}
+
+func assertExpression(t *testing.T, exp parser.Node, evaluator OperatorRunnable, expected parser.EvalResult) {
+	res := evaluator.Evaluate(&exp)
+
+	if res != expected {
+		t.Errorf("Expected %v but got '%v'", expected.ToString(), res.ToString())
 	}
 }
